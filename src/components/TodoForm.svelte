5e601b24cfa6c93209f121a36ea2395f6692a2b9
Supachai Wongmoon <supachai@outlook.com>
2021-03-28 15:41:04 +0700

    disabled button when empty

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -5,7 +5,8 @@
 
     const styles = {
         input: 'px-3 rounded-full py-2 border border-gray-200 w-full focus:outline-none focus:ring-2 focus:ring-purple-500',
-        button: 'px-2 py-2 rounded-full bg-purple-500 text-white w-full'
+        button: 'px-2 py-2 rounded-full bg-purple-500 text-white w-full',
+        buttonDisabled: ' opacity-50'
     }
 
     let message = ''
@@ -23,6 +24,6 @@
         </div>
     </div>
     <div class="w-1/5">
-        <button class={ styles.button }>Add</button>
+        <button disabled={ !message.length } class={ styles.button + (!message.length ? styles.buttonDisabled : '') }>Add</button>
     </div>
 </form>
